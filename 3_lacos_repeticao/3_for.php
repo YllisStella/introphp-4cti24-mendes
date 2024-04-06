90d5ad8c35b2b2ef1c28630035a3a642d2d45485
Prof Alan Reis <98103226+ReiSalan94@users.noreply.github.com>
2024-04-05 21:13:26 -0300

    Add files via upload

---
+++ 3_lacos_repeticao/3_for.php
@@ -0,0 +1,12 @@
+<?php
+ /* inicio do loop - condição - contador */
+    for($a = 1; $a <= 10; $a++){
+        $cubo = $a * $a * $a;
+        echo "O cubo de $a é $cubo <br>";
+    }
+
+    for($soma = 1; $soma <= 300; $soma++){
+        $quantos = $soma + 1;
+        echo "Fala 300! Contra $soma <br>";
+    }
+?>
